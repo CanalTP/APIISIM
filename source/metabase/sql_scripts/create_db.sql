140d7b70cb2d21009d0ff0d761d6a380c8b28629
ddfidox <ddfidox@gmail.com>
2014-04-29 14:09:13 +0200

    Update SQL scripts to conform to new specifications (version 1.6.1).

--- source/metabase/sql_scripts/create_db.sql
+++ source/metabase/sql_scripts/create_db.sql
@@ -11,27 +11,37 @@ CREATE TABLE mis(
     api_key varchar(50),
     start_date timestamp,
     end_date timestamp,
-    accessibility boolean,
-    car_feeder boolean,
-    bike_feeder boolean,
+    geographic_position_compliant boolean,
     multiple_start_and_arrivals boolean
 );
+
+CREATE TYPE transport_mode_enum AS ENUM ('all', 'bus', 'trolleybus', 'tram', 'coach', 'rail',
+                               'intercityrail', 'urbanrail', 'metro', 'air', 'water',
+                               'cable', 'funicular', 'taxi', 'bike', 'car');
+
+CREATE TYPE stop_type_enum AS ENUM ('GL', 'LAMU', 'ZE');
 
 CREATE TABLE stop(
     id serial PRIMARY KEY,
     code varchar(50) NOT NULL,
-    mis_id integer REFERENCES mis(id) NOT NULL,
+    mis_id integer REFERENCES mis(id) ON DELETE CASCADE NOT NULL,
     name varchar(255) NOT NULL,
     lat double precision NOT NULL,
-    long double precision NOT NULL
+    long double precision NOT NULL,
+    type stop_type_enum,
+    administrative_code varchar(255),
+    parent_id integer REFERENCES stop(id),
+    transport_mode transport_mode_enum,
+    quay_type varchar(255),
+    UNIQUE(code, mis_id)
 );
 
 CREATE TYPE transfer_status_enum AS ENUM ('auto', 'manual', 'blocked', 'moved');
 
 CREATE TABLE transfer(
     id serial PRIMARY KEY,
-    stop1_id integer REFERENCES stop(id) NOT NULL,
-    stop2_id integer REFERENCES stop(id) NOT NULL,
+    stop1_id integer REFERENCES stop(id) ON DELETE CASCADE NOT NULL,
+    stop2_id integer REFERENCES stop(id) ON DELETE CASCADE NOT NULL,
     distance integer NOT NULL,
     duration integer NOT NULL,
     prm_duration integer,
@@ -42,28 +52,22 @@ CREATE TABLE transfer(
 
 CREATE TABLE mis_connection(
     id serial PRIMARY KEY,
-    mis1_id integer REFERENCES mis(id) NOT NULL,
-    mis2_id integer REFERENCES mis(id) NOT NULL,
+    mis1_id integer REFERENCES mis(id) ON DELETE CASCADE NOT NULL,
+    mis2_id integer REFERENCES mis(id) ON DELETE CASCADE NOT NULL,
     start_date timestamp,
     end_date timestamp,
-    coeffcient real,
     UNIQUE(mis1_id, mis2_id)
 );
 
-CREATE TYPE mode_enum AS ENUM ('all', 'bus', 'trolleybus', 'tram', 'coach', 'rail',
-                               'intercityrail', 'urbanrail', 'metro', 'air', 'water',
-                               'cable', 'funicular', 'taxi', 'bike', 'car');
-
 CREATE TABLE mode(
     id serial PRIMARY KEY,
-    code mode_enum UNIQUE NOT NULL,
-    description varchar(255)
+    code transport_mode_enum UNIQUE NOT NULL
 );
 
 CREATE TABLE mis_mode(
     id serial PRIMARY KEY,
-    mis_id integer REFERENCES mis(id) NOT NULL,
-    mode_id integer REFERENCES mode(id) NOT NULL,
+    mis_id integer REFERENCES mis(id) ON DELETE CASCADE NOT NULL,
+    mode_id integer REFERENCES mode(id) ON DELETE CASCADE NOT NULL,
     UNIQUE(mis_id, mode_id)
 );
 
--- source/metabase/sql_scripts/populate_db.sql
+++ source/metabase/sql_scripts/populate_db.sql
@@ -2,13 +2,13 @@
 BEGIN;
 
 INSERT INTO mis (name, comment, api_url, api_key, start_date, end_date, 
-                 accessibility, car_feeder, bike_feeder, multiple_start_and_arrivals) VALUES
+                 geographic_position_compliant, multiple_start_and_arrivals) VALUES
     ('mis1', 'comment1', 'url1', 'key1', TIMESTAMP '2011-05-16 15:36:38', 
-     TIMESTAMP '2016-05-16 15:36:38', TRUE, TRUE, FALSE, FALSE),
+     TIMESTAMP '2016-05-16 15:36:38', TRUE, FALSE),
     ('mis2', 'comment2', 'url2', 'key2', TIMESTAMP '2008-02-19 15:36:38', 
-     TIMESTAMP '2014-04-16 15:36:38', TRUE, FALSE, TRUE, FALSE),
+     TIMESTAMP '2014-04-16 15:36:38', FALSE, FALSE),
     ('mis3', 'comment3', 'url3', 'key3', TIMESTAMP '2012-12-11 15:36:38', 
-     TIMESTAMP '2018-08-01 15:36:38', FALSE, FALSE, TRUE, TRUE);
+     TIMESTAMP '2018-08-01 15:36:38', TRUE, TRUE);
 
 INSERT INTO stop (code, mis_id, name, lat, long) VALUES
     ('stop_code1', 1, 'stop1', 11, 111),
@@ -20,24 +20,21 @@ INSERT INTO transfer (stop1_id, stop2_id, distance, duration, prm_duration, stat
     (2, 3, 140, 1, 40, 'manual'),
     (3, 1, 260, 20, 66,'blocked');
 
-INSERT INTO mis_connection (mis1_id, mis2_id, start_date, end_date, coeffcient) VALUES
+INSERT INTO mis_connection (mis1_id, mis2_id, start_date, end_date) VALUES
     (1, 2, 
      (SELECT GREATEST((SELECT start_date from mis where id=1), (SELECT start_date from mis where id=2))), 
-     (SELECT LEAST((SELECT end_date from mis where id=1), (SELECT end_date from mis where id=2))), 
-     0.5),
+     (SELECT LEAST((SELECT end_date from mis where id=1), (SELECT end_date from mis where id=2)))),
     (2, 3,
      (SELECT GREATEST((SELECT start_date from mis where id=2), (SELECT start_date from mis where id=3))), 
-     (SELECT LEAST((SELECT end_date from mis where id=2), (SELECT end_date from mis where id=3))),  
-     0.1),
+     (SELECT LEAST((SELECT end_date from mis where id=2), (SELECT end_date from mis where id=3)))),
     (1, 3, 
      (SELECT GREATEST((SELECT start_date from mis where id=1), (SELECT start_date from mis where id=3))), 
-     (SELECT LEAST((SELECT end_date from mis where id=1), (SELECT end_date from mis where id=3))),
-     1);
+     (SELECT LEAST((SELECT end_date from mis where id=1), (SELECT end_date from mis where id=3))));
 
-INSERT INTO mode (code, description) VALUES
-    ('bus', 'bus_desc'),
-    ('all', 'all_desc'),
-    ('funicular', 'funicular_desc');
+INSERT INTO mode (code) VALUES
+    ('bus'),
+    ('all'),
+    ('funicular');
 
 INSERT INTO mis_mode (mis_id, mode_id) VALUES
     (1,1),
