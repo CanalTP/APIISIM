f5271683f602b618025971146cb167aefeb64051
ddfidox <ddfidox@gmail.com>
2014-05-09 11:45:16 +0200

    Add back_office component. This version is working fine but:     - it is not optimized (can eat lots of RAM if there is a big number of stops/transfers)     - it needs improved logging and statistsics     - it needs configuration  via command-line or at least via separate config files     - there are no unit tests yet

---
+++ source/back_office/mis_api.py
@@ -0,0 +1,45 @@
+import httplib2, json, logging
+
+HTTP_OK = 200
+
+class Stop():
+    def __init__(self, code, name, lat, long):
+        self.code = code
+        self.name = name
+        self.lat = lat
+        self.long = long
+
+    def __repr__(self):
+        return ("<Stop: code=%s, name=%s, lat=%s, long=%s>" % \
+                (self.code, self.name , self.lat , self.long)).encode("utf-8")
+
+class MisApi():
+    def __init__(self, api_url, api_key=""):
+        self._api_url = api_url
+        self._api_key = api_key
+
+    # Return a list with all stop points from this mis
+    def get_stops(self):
+        h = httplib2.Http(".cache")
+        base_url = self._api_url + "stops/"
+        params = ""
+        stops = []
+        if params:
+            url = base_url + ("&" if "?" in base_url else "?") + params
+        else:
+            url = base_url
+        logging.debug(url)
+        resp, content = h.request(url, "GET")
+        if resp.status != HTTP_OK:
+            raise Exception("[FAIL]: GET %s: %s" % (url, resp.status))
+
+        content = json.loads(content)
+        logging.debug(content)
+        for s in content["stops"]:
+            logging.debug(s)
+            stops.append(Stop(code=s["code"],
+                              name=s["name"],
+                              lat=float(s["lat"]),
+                              long=float(s["long"])))
+
+        return stops
