e20e01ad06cee62f700af8df66821a18d680472c
ddfidox <ddfidox@gmail.com>
2014-07-31 18:04:16 +0200

    Add setup script.

---
+++ source/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='apiisim',
+      version='0.1',
+      description='Trip planning between multiple heterogeneous'
+                  'multi-modal information systems (MIS)',
+      author='CanalTP',
+      packages=find_packages()
+)
